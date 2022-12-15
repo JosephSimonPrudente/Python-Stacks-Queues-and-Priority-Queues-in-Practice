bb4ef5976256d296f2b38a52bf62e30796a36078
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-12-16 03:37:23 +0800

    adding familiar problem

--- Data class.py
+++ Data class.py
@@ -9,3 +9,9 @@ wipers = Message("Windshield wipers turned on")
 hazard_lights = Message("Hazard lights turned on")
 
 print(wipers < hazard_lights)
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
+
+print(messages.enqueue_with_priority(CRITICAL, Message("ABS engaged")))
