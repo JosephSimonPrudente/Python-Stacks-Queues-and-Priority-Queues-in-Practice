24f9507bc8bacff94ff7a6161d97a2c2f90171e9
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-12-16 02:50:08 +0800

    tuple comparison

--- heap.py
+++ heap.py
@@ -11,3 +11,11 @@ from heapq import heappop
 heappop(fruits)
 
 print(fruits)
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+print(person1 < person2)
+
+print(person2 < person3)
