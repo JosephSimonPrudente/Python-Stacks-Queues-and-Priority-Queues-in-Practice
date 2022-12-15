2e99a821cb2ef5f0d5ab12ac9d5f4b46d1831383
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-12-16 02:31:54 +0800

    changing the placement of class stack

---
+++ LIFO stack.py
@@ -0,0 +1,5 @@
+from queues1 import Stack
+
+lifo = Stack("1st", "2nd", "3rd")
+for element in lifo:
+    print(element)
--- queues.py
+++ queues.py
@@ -9,7 +9,3 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
-
-class Stack(Queue):
-    def dequeue(self):
-        return self._elements.pop()
