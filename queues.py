08ddd631e751bf449c59022a5500dda786bb4ac5
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-12-16 03:43:09 +0800

    adding new priority queues class with the use of itertools

--- queues.py
+++ queues.py
@@ -1,5 +1,6 @@
 from collections import deque
 from heapq import heappop, heappush
+from itertools import count
 
 class Queue:
     def __init__(self):
@@ -30,3 +31,15 @@ class PriorityQueue1:
 
     def dequeue(self):
         return heappop(self._elements)[1]
+
+class PriorityQueue2:
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
