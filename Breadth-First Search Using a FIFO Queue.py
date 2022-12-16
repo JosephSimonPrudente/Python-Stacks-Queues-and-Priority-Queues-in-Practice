1431fde263ea6213dd196548332138e6b4d62eb8
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-12-16 22:29:49 +0800

    neighbors according to some criteria

--- Breadth-First Search Using a FIFO Queue.py
+++ Breadth-First Search Using a FIFO Queue.py
@@ -13,4 +13,16 @@ for node in nx.bfs_tree(graph, nodes["edinburgh"]):
 else:
     print("Not found")
 print("_________________________________________________")
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
 
+for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
+print("_________________________________________________")
