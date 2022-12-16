bfe8a43267d602858f822cc5a05c31e023a17090
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-12-16 23:58:05 +0800

    moving queue1 to queues

--- queues.py
+++ queues.py
@@ -12,6 +12,28 @@ class Queue:
     def dequeue(self):
         return self._elements.popleft()
 
+class Queue1:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
 class PriorityQueue:
     def __init__(self):
         self._elements = []
