dd30abd66736b2577d6c4976c09ee92afd157de9
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-12-16 22:22:24 +0800

    example 3 and dividing the example

--- graph 1.py
+++ graph 1.py
@@ -1,11 +1,14 @@
 from graph import City, load_graph
-
+print("_________________________________________________")
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 print(nodes["london"])
 
 print(graph)
-
+print("_________________________________________________")
 for neighbor in graph.neighbors(nodes["london"]):
     print(neighbor.name)
-
+print("_________________________________________________")
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
+print("_________________________________________________")
